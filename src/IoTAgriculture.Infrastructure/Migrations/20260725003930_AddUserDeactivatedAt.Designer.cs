55bc205dfebdec18c545a9303a653245f45f0576
Shuichi2504 <DuyKhanh2504k@gmail.com>
2026-07-25 08:48:52 +0700

    Add files via upload

--- src/IoTAgriculture.Infrastructure/Migrations/20260725003930_AddUserDeactivatedAt.Designer.cs
+++ src/IoTAgriculture.Infrastructure/Migrations/20260725003930_AddUserDeactivatedAt.Designer.cs
@@ -4,6 +4,7 @@ using IoTAgriculture.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IoTAgriculture.Infrastructure.Migrations
 {
     [DbContext(typeof(IoTDbContext))]
-    partial class IoTDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260725003930_AddUserDeactivatedAt")]
+    partial class AddUserDeactivatedAt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
